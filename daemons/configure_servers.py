4f14f093ac8d1c3d5bdf005a3079d524f877d4f6
ehsan <ehsan.shirzadi@gmail.com>
2021-09-06 19:22:23 +0430

    A few enhancements in configure servers

--- daemons/configure_servers.py
+++ daemons/configure_servers.py
@@ -24,26 +24,30 @@ col_ha = db['ha']
 
 def config_master(cluster_name, ha_ip):
     servers = col_server.find({'cluster_name': cluster_name, 'role': {'$in': ['master', 'main_master']}, 'status': {'$nin': ['done', 'pending']}, 'ip': {'$ne': ''}})
-    print({'cluster_name': cluster_name, 'role': {'$in': ['master', 'main_master']}, 'status': {'$nin': ['done', 'pending']}, 'ip': {'$ne': ''}})
+    # print({'cluster_name': cluster_name, 'role': {'$in': ['master', 'main_master']}, 'status': {'$nin': ['done', 'pending']}, 'ip': {'$ne': ''}})
     print(f'{servers.count()} servers found to config as masters')
     masters = []
     main_master = {}
+    main_master_configured = False
     for server in servers:
-        print(server['name'], server['role'])
+        # print(server['name'], server['role'])
         if server['role'] == 'main_master':
             main_master = {'name': server['name'], 'ip': server['ip']}
+            main_master_configured = server['status'] == 'done'
         elif server['role'] == 'master':
             masters.append({'name': server['name'], 'ip': server['ip']})
-    print(masters)
-    print(main_master)
+    # print(masters)
+    # print(main_master)
     #for master in cluster_info['masters']:
       #if '0' in master['name']:
     if main_master == {}:
         print('No unconfigured main master detected!')
+    elif main_master_configured:
+        print('Main master already configured!')
     else:
         col_server.update_one({'ip': main_master['ip']}, {'$set': {'status': 'pending'}})
         try:
-          print('========================================')
+          # print('========================================')
           os.system(' ssh-keygen -f "/home/ubuntu/.ssh/known_hosts" -R "%s"' % main_master['ip'])
           command = "ansible-playbook ../playbooks/activate-masters.yml -e 'ha_ip=%s' -i %s," % (ha_ip, main_master['ip'])
           print(command)
@@ -51,7 +55,7 @@ def config_master(cluster_name, ha_ip):
           print(output)
           col_server.update_one({'ip': main_master['ip']}, {'$set': {'status': 'done'}})
         except:
-          cluster_error = True
+          # cluster_error = True
           col_server.update_one({'ip': main_master['ip']}, {'$set': {'status': 'error'}})
   
   
@@ -63,10 +67,11 @@ def config_master(cluster_name, ha_ip):
     if main_master['ip'] == '':
         print('Main master have not an IP')
         exit()
+
     print('Going to get a token...')
     get_token(main_master['ip'])
     print('Tokens are stored')
-    print(masters)
+    # print(masters)
     if masters == []:
         print('No unconfigured masters detected!')
     else:
@@ -88,8 +93,9 @@ def config_master(cluster_name, ha_ip):
             col_server.update({'ip': {'$in': ip_list}}, {'$set': {'status': 'done'}}, multi=True)
       
         except:
-          cluster_error = True
+          # cluster_error = True
           col_server.update({'ip': {'$in': ip_list}}, {'$set': {'status': 'error'}}, multi=True)
+
 
 def get_token(main_master_ip):
     command = "ansible-playbook ../playbooks/token.yml -i %s," % main_master_ip
@@ -98,41 +104,8 @@ def get_token(main_master_ip):
     print(output)
 
 
-#def config_other_masters():
-#  #print(cluster_info) 
-#  ips = ""
-#  ip_list = [] 
-#  for master in cluster_info['masters']:
-#    if '0' not in master['name']:
-#      col_server.update_one({'ip': master['ip']}, {'$set': {'status': 'pending'}})
-#      os.system(' ssh-keygen -f "/home/ubuntu/.ssh/known_hosts" -R "%s"' % master['ip'])
-#      ips+= master['ip'] + ","
-#      ip_list.append(master['ip'])
-#  print(ips)
-#   
-#  try:
-#    command = "ansible-playbook ../playbooks/join-master.yml -e 'ha_ip=%s' -i %s" % (cluster_info['masters_ha'], ips)
-#    print(command)
-#    if ips != ",":
-#      output = subprocess.check_output(command, shell=True).decode()
-#      print(output)
-#      col_server.update({'ip': {'$in': ip_list}}, {'$set': {'status': 'done'}}, multi=True)
-#
-#  except:
-#    col_server.update({'ip': {'$in': ip_list}}, {'$set': {'status': 'error'}}, multi=True)
-
-
 def join_workers(cluster_name):
     servers = col_server.find({'cluster_name': cluster_name, 'role': 'worker', 'status': {'$nin': ['done', 'pending']}, 'ip': {'$ne': ''}})
-
-#    command = "ansible-playbook ../playbooks/token.yml -i %s," % cluster_info['masters'][0]['ip']
-#    print(command)
-#    if print_mode:
-#      print(command)
-#    else:
-#      output = subprocess.check_output(command, shell=True).decode()
-#
-#    worker = get_free_server(CLUSTER_NAME, 'Worker')
     if servers.count_documents() == 0:
         print('No workers to joins')
     else:
@@ -149,7 +122,7 @@ def join_workers(cluster_name):
             output = subprocess.check_output(command, shell=True).decode()
             col_server.update({'ip': {'$in': ip_list}}, {'$set': {'status': 'done'}}, multi=True)
         except:
-          cluster_error = True
+          # cluster_error = True
           col_server.update({'ip': {'$in': ip_list}}, {'$set': {'status': 'error'}}, multi=True)
 
 print('HELLO!')
