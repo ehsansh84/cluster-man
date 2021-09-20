d4268ae3b829ae0bccb0e1998cdd56f59a4122ca
Ehsan <ehsan.shirzadi@gmail.com>
2021-09-20 17:15:12 +0430

    Set mongo.yml networkdefault

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ docker rmi cman || true
 docker build -t cman .
 docker stop cman || true
 docker rm cman || true
-docker run --name cman -p 8101:8282 -d --restart always --network dockers_default -e MONGO=mongodb  cman
+docker run --name cman -p 8101:8282 -d --restart always --network cluster-man_default -e MONGO=mongodb  cman
 
 
